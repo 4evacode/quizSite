db028fea66a0926ab6203f64a8349bc87d9f5373
Addai <119640046+4evacode@users.noreply.github.com>
2023-03-18 10:34:39 +0000

    Add files via upload

---
+++ question.js
@@ -0,0 +1,50 @@
+let questions = [
+    {
+        numb: 1,
+        question: 'How much money do you need in life?',
+        answer: '$Above $10,000',
+        options: [
+            'Under $1,000',
+            '$1,000 - $5,000',
+            '$5,000 - $10,000',
+            '$Above $10,000'
+        ]
+    },
+
+    {
+        numb: 2,
+        question: 'what do you know about investing?',
+        answer: 'little to nothing',
+        options: [
+            'investing is the way of life',
+            'resign from your day job',
+            'why this question',
+            'little to nothing'
+        ]
+    },
+
+    {
+        numb: 3,
+        question: 'Will you ever marry?',
+        answer: 'not in this life',
+        options: [
+            'not in this life',
+            'yes gladly',
+            'somethings arent for everyone',
+            'accontants will thrive'
+        ]
+    },
+
+    {
+        numb: 4,
+        question: 'Do you think life is fair?',
+        answer: 'Not at all but we move',
+        options: [
+            'Sometimes',
+            'life is hard',
+            'Not at all but we move',
+            'The answer isnt here'
+        ]
+    },
+    
+]
